b00f2435c54a558f4b094bd313a9bcf8b574543d
Beatrice <beatrice.trinidad@gmail.com>
2018-10-19 01:31:47 -0400

    Updated db credentials

--- private/db_credentials.php
+++ private/db_credentials.php
@@ -1,6 +1,6 @@
 <?php
-define("DB_SERVER", "127.0.0.1");
-define("DB_USER", "webuser");
-define("DB_PASS", "user123");
-define("DB_NAME", "village_med");
+define("DB_SERVER", "mysql.syk.dreamhosters.com");
+define("DB_USER", "village_med");
+define("DB_PASS", "village_med_1010");
+define("DB_NAME", "villagemed");
 ?>
